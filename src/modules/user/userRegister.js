1d7dbe45a40f67db131bd849d93eaa701675cba1
dfavo <favorskyd@gmail.com>
2021-12-05 18:59:47 -0800

    User Create

--- src/modules/user/userRegister.js
+++ src/modules/user/userRegister.js
@@ -1,10 +1,20 @@
 // WHEN SERVER IS WORKING WE CAN STORE SOME DATA IN IT 
 // WHEN SERVER STOP WORKING DATA DISAPPEAR. WE STORE DATA IN DATABASE
+const User = require("./Model");
 
 function userRegister(req, res) {
+  const newUser = new User({
+    email: req.body.email,
+    password: req.body.password,
+
+  });
+
+  newUser.save();
+
   res.status(200).json("userRegister here");
   
 }
 
+module.exports = userRegister;
 
-module.exports = (userRegister);
+
