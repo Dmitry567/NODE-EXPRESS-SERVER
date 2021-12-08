00df0e060f2523d935e0776bb1951842b17af75c
dfavo <favorskyd@gmail.com>
2021-12-07 20:43:06 -0800

    User Get ById

--- src/modules/user/Routes.js
+++ src/modules/user/Routes.js
@@ -1,12 +1,14 @@
 const Router = require("express");
 const userRegister = require("./userRegister");
 const userGetAll = require("./userGetAll");
+const userGetById = require("./userGetById");
 
 
 const router = Router();
 
 router.post("/", userRegister);// POST localhost:5000/user
 router.get("/", userGetAll);// GET localhost:5000/get all users
+router.get("/:userId", userGetById);// GET localhost:5000/61ae5bd58193deb92141e3ff
 
 
 module.exports = router;
